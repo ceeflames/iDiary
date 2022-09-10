29d3ecdf1e04f3a44d7825910737a6d6290366ec
Flames <ceeflames@yahoo.com>
2022-09-10 22:15:05 +0800

    few settings changes

--- iDiary/settings.py
+++ iDiary/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lists',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists',
 ]
 
 MIDDLEWARE = [
